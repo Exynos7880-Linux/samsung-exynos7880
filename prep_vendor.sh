9cb8f6d143f51684166defcbbacefc523f27e0f8
aymanrgab <aymanrar2c@gmail.com>
2022-12-16 22:10:55 +0200

    update

--- prep_vendor.sh
+++ prep_vendor.sh
@@ -3,7 +3,7 @@
 set -xe
 
 prep_vendor () {
-	e2fsck -fy ${1}_installer/data/vendor.img
+	e2fsck -fp ${1}_installer/data/vendor.img
 	resize2fs -d -f ${1}_installer/data/vendor.img 230M
 	mount ${1}_installer/data/vendor.img vendor
 	mv -vf patches/build_${1}.prop vendor/build.prop
