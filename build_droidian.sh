f74b861301eb51775ddf5ba319a73b1e9736f202
aymanrgab <aymanrar2c@gmail.com>
2023-04-23 01:05:25 +0200

    using prebuilt droidian ramdisk

--- build_droidian.sh
+++ build_droidian.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -xe
-ARCH=arm64
-
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-10-focal build
 mkdir -p dr/downloads
-git clone https://github.com/droidian/initramfs-tools-halium.git
-cd initramfs-tools-halium
-sudo ./build-initrd.sh -a $ARCH
-cd ../
-cp ./initramfs-tools-halium/out/initrd.img-touch-$ARCH ./dr/downloads/halium-boot-ramdisk.img
-rm -rf initramfs-tools-halium
+mv initrd.img-halium-generic ./dr/downloads/halium-boot-ramdisk.img
 ./build/build.sh "$@"
 
